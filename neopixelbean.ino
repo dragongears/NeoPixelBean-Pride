685a4aff3d8751b9edbd6d428e37d5c4b5eed4cf
Art Dahm <adahm@pearlholding.com>
2017-05-25 13:18:41 -0400

    Button library

--- neopixelbean.ino
+++ neopixelbean.ino
@@ -1,15 +1,23 @@
 #include <Adafruit_NeoPixel.h>
+#include <Button.h>
 #include "Easing.h"
 
 #define PIN 5
 #define NUMPIXELS 12
-#define SPEED 80
 #define BRIGHTNESS 60
+
+// Easing Loop
+#define SPEED 80
 #define DURATION 12 * 80
 #define RANGE 12 * 80
 
+// Display mode
+int mode = 0;
+
+int j;
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+Button button(0);
 
 const uint16_t colors[][3] = {
     {255, 0, 0}, //Red
@@ -49,18 +57,98 @@ void displayRainbow(int offset) {
     pixels.show();
 }
 
+void modeSetup() {
+    switch (mode) {
+        case 0:
+            easingSetup();
+            break;
+
+        case 1:
+            cycleSetup();
+            break;
+    }
+}
+
+void easingSetup() {
+    j = -DURATION;
+    displayRainbow(Easing::easeInOutSine(abs(j), 0, RANGE, DURATION));
+}
+
+void easingLoop() {
+    if (j < DURATION -12) {
+        displayRainbow(Easing::easeInOutSine(abs(j), 0, RANGE, DURATION));
+
+//        Bean.sleep(SPEED);
+        delay(SPEED);
+
+        j++;
+    } else {
+        j = -DURATION;
+    }
+}
+
+void displayColor(int color) {
+    color = color + color;
+
+    for(int i = 0; i < NUMPIXELS; i++) {
+        pixels.setPixelColor(i, pixels.Color(colors[color][0], colors[color][1], colors[color][2]));
+    }
+
+    pixels.show();
+}
+
+void cycleSetup() {
+    j = 0;
+    displayColor(j);
+}
+
+void cycleLoop() {
+    if (j < 6) {
+        displayColor(j);
+
+//        Bean.sleep(SPEED * 5);
+        delay(SPEED * 5);
+
+        j++;
+    } else {
+        j = 0;
+    }
+}
+
 void setup() {
+    pinMode(0, INPUT_PULLUP);
+    button.begin();
+
     pixels.begin();  //  Initialize the NeoPixels
     pixels.setBrightness(BRIGHTNESS);
-    displayRainbow(0);
+
+    modeSetup();
 }
 
 void loop() {
-    for(int j = -DURATION; j < DURATION -12; j++) {
+    switch (mode) {
+        case 0:
+            easingLoop();
+            break;
 
-    displayRainbow(Easing::easeInOutSine(abs(j), 0, RANGE, DURATION));
-
-    Bean.sleep(SPEED);
+        case 1:
+            cycleLoop();
+            break;
     }
+
+    if (button.pressed()) {
+        buttonPressed();
+    }
+}
+
+// Interrupt service routine (ISR) needs to return void and accept no arguments
+void buttonPressed() {
+    mode++;
+
+    if (mode >= 2) {
+        mode = 0;
+    }
+
+    modeSetup();
 
 }
